37aed1c571cd592c24bc9c6a57ed269e8998a0d7
ruhi <raktterruhi@gmail.com>
2022-07-01 01:05:23 +0600

    data loaded

--- index.js
+++ index.js
@@ -3,13 +3,28 @@ const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
-const port = process.env.PORT || 5000;
+const jwt = require("jsonwebtoken");
+const port = process.env.PORT || 5000; 
 
 //Using the Middleware ---
 app.use(cors());
 app.use(express.json());
 
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "unauthorized access" });
+    }
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+        return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+    });
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q3ms5ct.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -22,9 +37,22 @@ async function run (){
     try{
         await client.connect();
         const spicesCollection = client.db("spicesCollection").collection("spices");
-        const spices = {name:'oregano', price: '23' };
-        const result = await spicesCollection.insertOne(spices);
-        console.log(`user inserted with id: ${result.insertedId}`);
+
+         // AUTH
+        app.post("/login", async (req, res) => {
+        const user = req.body;
+        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: "1d",
+        });
+        res.send({ accessToken });
+        });
+
+        //Fetching all the spices
+        app.get("/spices", async (req, res) =>{
+            const query = {};
+            const spices = await spicesCollection.find(query).toArray();
+            res.send(spices);
+        })
     }
     finally{
         // await client.close();
